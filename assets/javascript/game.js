25d8639cf32699eb382caa790834ed057e4450a5
Aaron Lanier <alanier.william@gmail.com>
2019-06-16 20:17:45 -0400

    added animal array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,4 @@
 console.log("test")
+
+var zooAnimals = ["Goat", "Sheep", "Pig", "Cow", "Zebu", "Donkey", "Buffalo", "Camel", "Horse", "Yak", "Llama", "Alpaca", "Rabbit", "Bison", "Deer", "Elk", "Guinea pig", "Mule", "Moose", "Muskox", "Reindeer"]
+// 
