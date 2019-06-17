bc7662bdff3684d6583539f6dba1ce56631214d7
Aaron Lanier <alanier.william@gmail.com>
2019-06-17 14:30:40 -0400

    changed name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
 console.log("test")
 
 var zooAnimals = ["Goat", "Sheep", "Pig", "Cow", "Zebu", "Donkey", "Buffalo", "Camel", "Horse", "Yak", "Llama", "Alpaca", "Rabbit", "Bison", "Deer", "Elk", "Guinea pig", "Mule", "Moose", "Muskox", "Reindeer"]
-// 
