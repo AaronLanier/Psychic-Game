952e869bb088db2837cee3b45c2266b50915c201
Aaron Lanier <alanier.william@gmail.com>
2019-06-17 23:44:36 -0400

    added javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,28 @@
 console.log("test")
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
 
 
-for (var i = 0; i < letters.length; i++){
-    console.log(letters[i])
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text")
+var guessesLeftText= document.getElementById("guesses-left")
+var yourGuesses = document.getElementById("your-guesses");
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if (userGuess === computerGuess){
+        console.log(test)
+        winsText.textContent = "wins: " + wins;
+
+        lossesText.textContent = "losses: " + losses;
+    }
 
 }
