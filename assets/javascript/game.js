012074cb4aaf5a299e89a5894e06cee1598bcb3f
Aaron Lanier <alanier.william@gmail.com>
2019-06-18 21:44:54 -0400

    added your guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,52 @@ var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
-
+// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var computerGuess = "A";
+console.log("computerGuess");
+console.log(computerGuess);
 var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text")
-var guessesLeftText= document.getElementById("guesses-left")
-var yourGuesses = document.getElementById("your-guesses");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left");
+var yourGuessesText = document.getElementById("your-guesses");
 
-document.onkeyup = function(event) {
+guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+winsText.textContent = "wins: " + wins;
+lossesText.textContent = "losses: " + losses;
+
+
+document.onkeyup = function (event) {
 
     var userGuess = event.key;
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(userGuess)
 
-    if (userGuess === computerGuess){
-        console.log(test)
+    if (userGuess == computerGuess) {
+
+        guessesLeft--;
+        wins++;
+
+        console.log("test")
         winsText.textContent = "wins: " + wins;
 
         lossesText.textContent = "losses: " + losses;
+
+        guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+
+        yourGuessesText.textContent = "Your Guesses: " + userGuess;
+
+    }
+    else if (userGuess !== computerGuess) {
+        guessesLeft--;
+        losses++;
+        console.log("test")
+        winsText.textContent = "wins: " + wins;
+
+        lossesText.textContent = "losses: " + losses;
+
+        guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+
+        yourGuessesText.textContent = "Your Guesses: " + userGuess;
     }
 
 }
